d367713a1b359eac99ea96989820c7988944e9b2
ALOKJENA876 <162704368+ALOKJENA876@users.noreply.github.com>
2025-07-05 23:23:06 -0500

    x

--- app/src/main/java/my/gradle/pipeline/App.java
+++ app/src/main/java/my/gradle/pipeline/App.java
@@ -5,7 +5,7 @@ package my.gradle.pipeline;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello Alok!";
     }
 
     public static void main(String[] args) {
